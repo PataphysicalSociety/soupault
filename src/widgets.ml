d0d64c549ef29fb057254c9c64360e21fdaf9b92
Daniil Baturin <daniil@baturin.org>
2019-07-03 09:28:03 +0700

    Move the title widget to a module for built-in widgets.

--- src/widgets.ml
+++ src/widgets.ml
@@ -7,34 +7,11 @@ type 'a widget = {
 (* Option monad *)
 let (>>=) = CCOpt.(>>=)
 
-(* Built-in widgets *)
-let set_title config soup =
-  (* Retrieve config options. The "selector" option means title source element, by default the first <h1> *)
-  let selector = Config.get_string_default "h1" "selector" config in
-  let prepend = Config.get_string_default "" "prepend" config in
-  let append = Config.get_string_default "" "append" config in
-  let default_title = Config.get_string_default "" "default" config in
-  (* Now to setting the title *)
-  let title_node = Soup.select_one "title" soup in
-  match title_node with
-  | None ->
-    let () = Logs.info @@ fun m -> m "Page has no <title> node, assuming you don't want to set it" in
-    Ok ()
-  | Some title_node ->
-    let title_string = Soup.select_one selector soup >>= Soup.leaf_text |> Config.default default_title in
-    let title_string = Printf.sprintf "%s%s" prepend title_string in
-    let title_string = Printf.sprintf "%s%s" title_string append in
-    (* XXX: Both Soup.create_text and Soup.create_element ~inner_text:... escape special characters
-       instead of expanding entities, so "&mdash;" becomes "&amp;mdash", which is not what we want.
-       Soup.parse expands them, which is why it's used here *)
-    let new_title_node = Printf.sprintf "<title>%s</title>" title_string |> Soup.parse in
-    let () = Soup.replace title_node new_title_node in
-    Ok ()
-
-
+(* Quick and dirty widget lookup *)
 let find_widget name =
-  match name with "title" -> Some set_title | _ -> None
-
+  try
+    Some (List.assoc name Builtin_widgets.widgets)
+  with Not_found -> None
 
 
 (* Widget config loading *)
