3f6d6ba8dee4d000e60cbc3f38256ef088aa379f
Daniil Baturin <daniil@baturin.org>
2019-07-05 10:00:39 +0700

    Configurable option for minimum depth where breadcrumbs appear.

--- src/builtin_widgets.ml
+++ src/builtin_widgets.ml
@@ -100,7 +100,7 @@ let make_breadcrumbs nav_path bc_tmpl_str prepend append between =
       let () = Soup.append_child bc_a (Soup.create_text x) in
       let () = Soup.append_root bc_soup bc_tmpl in
       (* Fixup: don't insert the "between" after the last element *)
-      let () = if (List.length xs) > 1 then Soup.append_root bc_soup (Soup.parse between) in
+      let () = if (List.length xs) >= 1 then Soup.append_root bc_soup (Soup.parse between) in
       aux xs bc_soup acc_href
   in
   let bc_soup = Soup.create_soup () in
@@ -116,9 +116,10 @@ let check_breadcrumb_template tmpl_str =
   let a = Soup.select_one "a" s in
   match a with
   | Some _ -> Ok ()
-  | None -> Error (Printf.sprintf "No <a> elements in breadcrumd template \"%s\", nowhere to set the link target" tmpl_str)
+  | None -> Error (Printf.sprintf "No <a> elements in breadcrumb template \"%s\", nowhere to set the link target" tmpl_str)
 
 let add_breadcrumbs env config soup =
+  let min_depth = Config.get_int_default 1 "min_depth" config in
   let selector = Config.get_string_result "Missing required option \"selector\"" "selector" config in
   match selector with
   | Error _ as e -> e
@@ -130,7 +131,7 @@ let add_breadcrumbs env config soup =
       | None -> Ok ()
       | Some container ->
         let path_length = List.length env.nav_path in
-        if path_length < 1 then Ok () else
+        if path_length < min_depth then Ok () else
         let bc_tmpl_str = Config.get_string_default "<a></a>" "breadcrumb_template" config in
         let%m _  = check_breadcrumb_template bc_tmpl_str in
         let prepend = Config.get_string_default "" "prepend" config in
