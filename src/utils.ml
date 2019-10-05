67a4e19ff3aba8edcf4b287b2d6b3d26cd98e5d8
Daniil Baturin <daniil@baturin.org>
2019-10-06 03:47:35 +0700

    Add PATH fixup on Win32 to allow calling external programs easily without having to specify an absolute path.

--- src/utils.ml
+++ src/utils.ml
@@ -10,6 +10,13 @@ let cp fs d =
 
 (** Executes an external program and returns its stdout *)
 let get_program_output ?(input=None) command env_array =
+  (* On Windows, child process environment is completely overwritten
+     if you use open_process_full, so we add %PATH% by hand here to allow
+     calling external programs *)
+  let env_array =
+    if Sys.os_type = "Win32" then Array.append env_array [|("PATH=" ^ Unix.getenv "PATH")|]
+    else env_array
+  in
   let std_out, std_in, std_err = Unix.open_process_full command env_array in
   let () =
     match input with
