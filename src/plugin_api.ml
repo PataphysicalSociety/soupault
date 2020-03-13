868c8cfc447cbbdab7b087c90a7d96c0607f5dae
Daniil Baturin <daniil@baturin.org>
2020-03-13 23:59:15 +0700

    Make the version comparison function aware of suffices. As of now it just ignores the suffix. In strict semver sense a version with a suffix is less than one without, but it's not clear if it's worth implementing or not.

--- src/plugin_api.ml
+++ src/plugin_api.ml
@@ -58,12 +58,12 @@ end
 
 module Plugin_version = struct
   let version_of_string vstr =
-    try Ok (Scanf.sscanf vstr "%u.%u.%u" (fun v1 v2 v3 -> (v1, v2, v3)))
-    with _ -> try Ok (Scanf.sscanf vstr "%u.%u" (fun v1 v2 -> (v1, v2, 0)))
-    with _ -> try Ok (Scanf.sscanf vstr "%u" (fun v1 -> (v1, 0, 0)))
+    try Ok (Scanf.sscanf vstr "%u.%u.%u" (fun v1 v2 v3 -> (v1, v2, v3, None)))
+    with _ -> try Ok (Scanf.sscanf vstr "%u.%u" (fun v1 v2 -> (v1, v2, 0, None)))
+    with _ -> try Ok (Scanf.sscanf vstr "%u" (fun v1 -> (v1, 0, 0, None)))
     with _ -> Error (Printf.sprintf "Could not parse version string \"%s\"" vstr)
 
-  let compare_versions (l1, l2, l3) (r1, r2, r3) =
+  let compare_versions (l1, l2, l3, _) (r1, r2, r3, _) =
     match compare l1 r1, compare l2 r2, compare l3 r3 with
     | 0, 0, res -> res
     | 0, res, _ -> res
