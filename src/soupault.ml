563f3020c30c9fa7c39b7f6a9866e1e8894d1e25
Daniil Baturin <daniil@baturin.org>
2020-06-19 09:11:44 +0700

    Disallow empty site_dir and move those warnings to one place.

--- src/soupault.ml
+++ src/soupault.ml
@@ -51,13 +51,6 @@ let list_section_files settings path =
   page_files, other_files
 
 let make_build_dir build_dir =
-  let () =
-    if build_dir = "" then
-    (* Treating build_dir="" as "build in the current dir" wasn't a part of the design.
-       I suppose it should be disabled in 2.0.
-     *)
-    Logs.warn @@ fun m -> m "Build directory is set to empty string, using current working directory for output"
-  in
   if (FU.test FU.Exists build_dir) then Ok () else
   let () = Logs.info @@ fun m -> m "Build directory \"%s\" does not exist, creating" build_dir in
   mkdir build_dir
@@ -381,14 +374,19 @@ let initialize () =
   } in
   let () =
     begin
-      let () = if not settings.generator_mode then
-        Logs.info @@ fun m -> m "Running in HTML processor mode, not using the page template"
-      in
-      let () = if settings.index_only && not (settings.index && (settings.dump_json <> None)) then
-        Logs.warn @@ fun m -> m "--index-only is useless without index=true and dump_json options in the config!"
-      in ()
+      if not settings.generator_mode then
+        Logs.info @@ fun m -> m "Running in HTML processor mode, not using the page template";
+      if settings.index_only && not (settings.index && (settings.dump_json <> None)) then
+        Logs.warn @@ fun m -> m "--index-only is useless without index=true and dump_json options in the config!";
+      if settings.build_dir = "" then
+      (* Treating build_dir="" as "build in the current dir" wasn't a part of the design.
+         I suppose it should be disabled in 2.0.
+       *)
+      Logs.warn @@ fun m -> m "Build directory is set to empty string, using current working directory for output"
     end
-  in Ok (config, widgets, settings, default_env)
+  in
+  if settings.site_dir = "" then (Error "site_dir must be a directory path, not an empty string")
+  else (Ok (config, widgets, settings, default_env))
 
 let dump_index_json settings index =
   match settings.dump_json with
