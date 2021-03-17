ef003748b9da4ce9d0deafed6c58da6b537549ee
Daniil Baturin <daniil@baturin.org>
2021-03-17 09:11:50 +0700

    [relative_links widget] Add only_target_regex option.

--- src/link_widgets.ml
+++ src/link_widgets.ml
@@ -2,9 +2,13 @@
 
 open Soupault_common
 
-let re_matches s re =
-  let ms = Re.matches re s in
-  List.length ms != 0
+let re_matches s pat =
+  try
+    let re = Re.Perl.compile_pat pat in
+    let ms = Re.matches re s in
+    List.length ms != 0
+  with Re__Perl.Parse_error | Re__Perl.Not_supported ->
+    soupault_error @@ Printf.sprintf "Malformed regex \"%s\"" pat
 
 let link_selectors = [ "a"; "link"; "img"; "script"; "audio"; "video"; "object"; "embed"]
 
@@ -23,15 +27,21 @@ let get_target_attr elem =
 
 let make_parent_path depth = Containers.String.repeat "../" depth
 
-let relativize elem env check_file exclude_regex =
+let relativize elem env check_file only_regex exclude_regex =
   let open Defaults in
+  let target_matches target =
+    (match only_regex with
+     | Some r -> re_matches target r
+     | None ->
+       not (re_matches target exclude_regex))
+  in
   let target_attr = get_target_attr elem in
   let target = Soup.attribute target_attr elem in
   match target with
   | None ->
     Logs.debug @@ fun m -> m "Ignoring a <%s> element without \"%s\" attribute" (Soup.name elem) target_attr
   | Some target ->
-    if (re_matches target exclude_regex)
+    if not (target_matches target)
     then Logs.debug @@ fun m -> m "Link target \"%s\" matches the exlude_regex, ignoring" target
     else begin
       (* Remove the build_dir from the path *)
@@ -48,21 +58,17 @@ let relativize elem env check_file exclude_regex =
 
 (** Converts all internal links to relative according to the page's location in the directory tree. *)
 let relative_links env config soup =
-  let valid_options = List.append Config.common_widget_options ["exclude_target_regex"; "check_file"] in
+  let valid_options = List.append Config.common_widget_options ["exclude_target_regex"; "only_target_regex"; "check_file"] in
   let () = Config.check_options valid_options config "widget \"relative_links\"" in
   let exclude_regex = Config.get_string_default "exclude_target_regex" "^((([a-zA-Z0-9]+):)|#)" config in
+  let only_regex = Config.get_string_opt "only_target_regex" config in
   let check_file = Config.get_bool_default false "check_file" config in
   let nodes = Html_utils.select_all link_selectors soup in
-  let re =
-    try Re.Perl.compile_pat exclude_regex
-    with Re__Perl.Parse_error | Re__Perl.Not_supported ->
-      soupault_error @@ Printf.sprintf "Invalid exclude_target_regex option: malformed regex \"%s\"" exclude_regex
-  in
   begin
     match nodes with
     | [] ->
        Logs.debug @@ fun m -> m "Page has no link elements that need adjustment"
-    | ns -> List.iter (fun e -> relativize e env check_file re) ns
+    | ns -> List.iter (fun e -> relativize e env check_file only_regex exclude_regex) ns
   end;
   Ok ()
 
