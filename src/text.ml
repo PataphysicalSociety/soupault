e4cb371a6e12c81857df892b5790bf9f0725f69e
Daniil Baturin <daniil@baturin.org>
2021-12-27 09:22:31 +0700

    Validate alleged unicode strings before trying to call unicode functions on them

--- src/text.ml
+++ src/text.ml
@@ -1,5 +1,12 @@
 open CamomileLibraryDefault
 
+let is_valid s =
+  try
+    let () = Camomile.UTF8.validate s in
+    true
+  with Camomile.UTF8.Malformed_code ->
+    false
+
 let sub s min max =
   let open Camomile in
   let rec aux buf s pos max =
@@ -8,8 +15,11 @@ let sub s min max =
     let () = UTF8.Buf.add_char buf c in
     aux buf s (pos + 1) max
   in
+  if not (is_valid s) then String.sub s min max else
   let buf = UTF8.Buf.create (max - min) in
   let () = aux buf s min max in
   UTF8.Buf.contents buf
 
-let length = Camomile.UTF8.length
+let length s =
+  if not (is_valid s) then String.length s else
+  Camomile.UTF8.length s
