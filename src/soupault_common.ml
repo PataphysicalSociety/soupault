a39ba8a2574f897756bbe4f7e43d36ed03ad4658
Daniil Baturin <daniil@baturin.org>
2022-04-03 12:14:30 +0300

    Add a new Internal_error exception to indicate unrecoverable internal errors that should produce exception traces

--- src/soupault_common.ml
+++ src/soupault_common.ml
@@ -1,3 +1,14 @@
 exception Soupault_error of string
-
 let soupault_error s = raise (Soupault_error s)
+
+(* This exception indicates an unrecoverable error in soupault's own logic:
+   unimplemented edge case, broken invariant etc.
+   It MUST NOT be ever handled so that the user can get a full exception trace
+   and either debug the problem or report it to the maintainers.
+ *)
+exception Internal_error of string
+let internal_error s =
+  let msg = Printf.sprintf "soupault encountered an internal error: %s.\
+    You can run soupault --debug to get an exception trace.\
+    Please report it as a bug!" s
+  in raise (Internal_error msg)
