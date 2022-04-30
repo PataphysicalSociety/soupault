397bb12914acfafdadc28259b3bd8a88deab85e1
Daniil Baturin <daniil@baturin.org>
2022-04-30 13:55:37 +0300

    [Link widgets] Completely separate link target handling from attribute handling to allow adding support for srcset and similar complex link attributes

--- src/link_widgets.ml
+++ src/link_widgets.ml
@@ -4,7 +4,7 @@ module OH = Otoml.Helpers
 
 open Soupault_common
 
-(* By default, exclude three categories of links from target rewriting:
+(* By default, exclude these categories of links from target rewriting:
      1. Links that have a URI schema (^([a-zA-Z0-9]+):), e.g. https://example.com
      2. Links to anchors within the same page (^#), e.g. #my-anchor
      3. Hand-made relative links (^\.), e.g. ../style.css
@@ -12,24 +12,73 @@ open Soupault_common
  *)
 let default_exclude_regex = "^((([a-zA-Z0-9]+):)|#|\\.|//)"
 
+(* Exhaustive list of elements supported by link widgets. *)
 let link_selectors = ["a"; "link"; "img"; "script"; "audio"; "video"; "object"; "embed"]
 
-let get_target_attr elem =
+(** Updates attribute's value using a target rewriting function:
+    relativize_link_target, absolutize_link_target, or a wrapper for one of those.
+
+    This is a generic function that, by itself, is good for use with attributes
+    that hold a single target, like "href" or "src".
+    In that case it can use relativize/absolutize_link_target functions directly
+    because they handle a single target.
+
+    For use with attributes that may have multiple targets, like "srcset",
+    it needs a wrapper that rewrites individual targets in the attribute value
+    using relativize/absolutize_link_target functions and reassembles
+    the attribute value with updated targets.
+  *)
+let process_attr func attr_name elem =
+  let attr_value = Soup.attribute attr_name elem in
+  match attr_value with
+  | None ->
+    (* No such attribute in the element, so there's nothing to do. *)
+    ()
+  | Some attr_value ->
+    let new_attr_value = func attr_value in
+    Soup.set_attribute attr_name new_attr_value elem
+
+(* HTML uses attributes with wildly different names for link targets.
+   Some elements may allow more than one possible attribute,
+   and some attributes may have multiple targets in them.
+   We have to maintain a map of element names to their allowed attributes
+   plus functions that can be used to process them.
+ *)
+let attribute_map = [
+  "a",      ["href", process_attr];
+  "link",   ["href", process_attr];
+  "script", ["src", process_attr];
+  "audio",  ["src", process_attr];
+  "video",  ["src", process_attr];
+  "embed",  ["src", process_attr];
+  "object", ["data", process_attr];
+  "img",    ["src", process_attr];
+]
+
+(** Iterates through all relevant attributes of an element
+    and applies appropriate attribute processing functions to them,
+    according to the [attribute_map].
+  *)
+let process_attrs attr_processor elem =
   let tag_name = Soup.name elem in
-  match tag_name with
-  | "a" -> "href"
-  | "link" -> "href"
-  | "img" -> "src"
-  | "script" -> "src"
-  | "audio" -> "src"
-  | "video" -> "src"
-  | "embed" -> "src"
-  | "object" -> "data"
-  | _ ->
-    (* Shouldn't happen *)
+  let mappings = List.assoc tag_name attribute_map in
+  match mappings with
+  | [] ->
+    (* Shouldn't happen because this function is called on elements selected using the [link_selectors] list *)
     internal_error @@ Printf.sprintf
       "a relative_links or an absolute_links widget tried to process unsupported element <%s>" tag_name
+  | _ ->
+     let aux = fun (attr_name, attr_processor_wrapper) -> attr_processor_wrapper attr_processor attr_name elem in
+     List.iter aux mappings
 
+(** Checks a target against regexes it should and should not match to qualify for rewriting.
+
+    The assumption is that [exclude_regex] is always defined: if the user doesn't supply one,
+    [default_exclude_regex] is used.
+
+    [only_regex] may or may not be defined, but if it's defined, then only links that match it
+    are subject to rewriting. If users specify an overly broad [only_regex], it's their responsibility.
+  *)
 let target_matches only_regex exclude_regex target =
   match only_regex with
   | Some r ->
@@ -53,8 +102,12 @@ let target_matches only_regex exclude_regex target =
    This function rewrites paths according to the real depth by adding a "../" for every nesting level
    to make relative paths correct.
  *)
-let relativize_link_target check_file env target =
+let relativize_link_target check_file env only_regex exclude_regex target =
   let open Defaults in
+  if not (target_matches only_regex exclude_regex target) then
+  let () =
+    Logs.debug @@ fun m -> m "Link target \"%s\" is excluded by regex options, not trying to make it relative" target
+  in target
   (* Before doing any real work, check if the link target is pointing at a file that actually exists
      at a path relative to _this_ page. If it does, the target is _already correct_
      and doesn't need to be relativized.
@@ -69,7 +122,7 @@ let relativize_link_target check_file env target =
        2. If a file is not in the site_dir, it doesn't mean it's not in the target_dir either.
           It may be a dynamically generated asset created by a Lua plugin or an external script.
    *)
-  if check_file && (Sys.file_exists (FilePath.concat env.target_dir target)) then target else
+  else if check_file && (Sys.file_exists (FilePath.concat env.target_dir target)) then target else
   (* Remove the build_dir from the target path to produce a path relative to the site root. *)
   let relative_target_dir = Regex_utils.Internal.replace ~regex:("^" ^ env.settings.build_dir) ~sub:"" env.target_dir in
   (* The assumption is that the target is valid for a page at the site root.
@@ -81,25 +134,15 @@ let relativize_link_target check_file env target =
   (* Prepend generated double-dot path to the original target. *)
   String.concat "/" (parent_path @ [target])
 
-let relativize elem env check_file only_regex exclude_regex =
-  let target_attr = get_target_attr elem in
-  let target = Soup.attribute target_attr elem in
-  match target with
-  | None ->
-    Logs.debug @@ fun m -> m "Ignoring a <%s> element without \"%s\" attribute" (Soup.name elem) target_attr
-  | Some target ->
-    if not (target_matches only_regex exclude_regex target)
-    then Logs.debug @@ fun m -> m "Link target \"%s\" is excluded by regex options, ignoring" target
-    else begin
-      let target = relativize_link_target check_file env target in
-      Soup.set_attribute (get_target_attr elem) target elem
-    end
-
 (** Prepends a prefix (typically the base URL of the website) to link targets. *)
-let absolutize_link_target prefix check_file env target =
+let absolutize_link_target prefix check_file env only_regex exclude_regex target =
   let open Defaults in
+  if not (target_matches only_regex exclude_regex target) then
+  let () =
+    Logs.debug @@ fun m -> m "Link target \"%s\" is excluded by regex options, not trying to make it absolute" target
+  in target
   (* Remove the build_dir from the path *)
-  let relative_target_dir = Regex_utils.Internal.replace ~regex:("^" ^ env.settings.build_dir) ~sub:"" env.target_dir in
+  else let relative_target_dir = Regex_utils.Internal.replace ~regex:("^" ^ env.settings.build_dir) ~sub:"" env.target_dir in
   (* Strip leading slashes *)
   let target = Regex_utils.Internal.replace ~regex:"^/+" ~sub:"" target in
   let parent_path =
@@ -112,20 +155,6 @@ let absolutize_link_target prefix check_file env target =
      else prefix)
    in
    String.concat "/" [parent_path; target]
-
-let absolutize elem env prefix check_file only_regex exclude_regex =
-  let target_attr = get_target_attr elem in
-  let target = Soup.attribute target_attr elem in
-  match target with
-  | None ->
-    Logs.debug @@ fun m -> m "Ignoring <%s> element without \"%s\" attribute" (Soup.name elem) target_attr
-  | Some target ->
-    if not (target_matches only_regex exclude_regex target)
-    then Logs.debug @@ fun m -> m "Link target \"%s\" matches exclude_target_regex, ignoring" target
-    else begin
-      let target = absolutize_link_target prefix check_file env target in
-      Soup.set_attribute (get_target_attr elem) target elem
-    end
 
 (** Converts all internal links to relative according to the page's location in the directory tree. *)
 let relative_links env config soup =
@@ -142,7 +171,9 @@ let relative_links env config soup =
     match nodes with
     | [] ->
       Logs.debug @@ fun m -> m "Page has no link elements that need adjustment"
-    | ns -> List.iter (fun e -> relativize e env check_file only_regex exclude_regex) ns
+    | ns ->
+      let relativize_target = relativize_link_target check_file env only_regex exclude_regex in
+      List.iter (fun elem -> process_attrs relativize_target elem) ns
     end;
     Ok ()
   end
@@ -168,7 +199,9 @@ let absolute_links env config soup =
     match nodes with
     | [] ->
       Logs.debug @@ fun m -> m "Page has no link elements that need adjustment"
-    | ns -> List.iter (fun e -> absolutize e env prefix check_file only_regex exclude_regex) ns
+    | ns ->
+      let absolutize_target = absolutize_link_target prefix check_file env only_regex exclude_regex in
+      List.iter (fun elem -> process_attrs absolutize_target elem) ns
     end;
     Ok ()
   end
