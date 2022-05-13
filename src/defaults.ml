f29b7ed54aa3f787ff00fae905a9b9779ed28cf7
Daniil Baturin <daniil@baturin.org>
2022-05-13 12:27:21 +0300

    4.0.0 release

--- src/defaults.ml
+++ src/defaults.ml
@@ -307,7 +307,7 @@ let default_settings = {
   soupault_version = None;
 }
 
-let version = (4, 0, 0, Some "beta3")
+let version = (4, 0, 0, None)
 
 let version_to_string v =
   let major, minor, patch, suffix = v in
