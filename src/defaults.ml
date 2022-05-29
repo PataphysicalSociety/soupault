d69fbd766cb1278d6faf76a9bb0ef0fedffe37a5
Daniil Baturin <daniil@baturin.org>
2022-05-29 11:22:41 +0300

    Update version to 4.0.1 for bug fixes

--- src/defaults.ml
+++ src/defaults.ml
@@ -307,7 +307,7 @@ let default_settings = {
   soupault_version = None;
 }
 
-let version = (4, 0, 0, None)
+let version = (4, 0, 1, None)
 
 let version_to_string v =
   let major, minor, patch, suffix = v in
