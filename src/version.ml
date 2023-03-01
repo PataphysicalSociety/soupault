5ad23036328ffa26d26a939ecf61e81a945ce10c
Daniil Baturin <daniil@baturin.org>
2023-03-01 22:51:24 +0000

    Improve comments in the version module

--- src/version.ml
+++ src/version.ml
@@ -1,21 +1,25 @@
 (* Version comparison *)
 
 (* Parses a version string.
-   The goal is to support full semver strings like 4.2.0-beta1
-   and also partial versions like 4 and 4.2,
-   since [Plugin.require_version(4)] is much shorter to write and, arguably, easier to read.
+
+   The goal is to support full semver strings like 4.2.0-beta1 (compliant)
+   and also partial versions like 4 (implied 4.0.0) and 4.2 (implied 4.2.0).
+
+   Partial versions are against the semver spec but
+   [Plugin.require_version(4)] is much shorter to write and, arguably, easier to read.
+
+   Soupault release versions are always semver-compliant,
+   it's just a shortcut for plugin writers.
  *)
 let version_of_string vstr =
-  (* The scanf approach won't be able to separate pre-release suffix and build metadata
-     in a case like 1.2.3-beta+20210919,
-     but since the semver.org standard says that build metadata must be ignored in comparisons,
-     we don't actually need to do it anyway. *)
+  (* Scanf is used here because it's easy to use and it's in the standard library.
+
+     That approach can't separate pre-release suffix and build metadata in a case like 1.2.3-beta+20210919.
+     However, the semver.org standard says that build metadata must be ignored in comparisons,
+     so we don't actually need to do it anyway.
+   *)
   try Ok (Scanf.sscanf vstr "%u.%u.%u-%s" (fun v1 v2 v3 v4 -> (v1, v2, v3, Some v4)))
   with _ -> try Ok (Scanf.sscanf vstr "%u.%u.%u" (fun v1 v2 v3 -> (v1, v2, v3, None)))
-  (* These options are provided for convenience, they are not semver-compliant.
-     Since there will never be, say, soupault "9.0-beta" (as opposed to "9.0.0-beta"),
-     they will not cause any trouble.
-   *)
   with _ -> try Ok (Scanf.sscanf vstr "%u.%u" (fun v1 v2 -> (v1, v2, 0, None)))
   with _ -> try Ok (Scanf.sscanf vstr "%u" (fun v1 -> (v1, 0, 0, None)))
   with _ -> Error (Printf.sprintf {|Could not parse version string "%s"|} vstr)
