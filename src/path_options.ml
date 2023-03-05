db6c76e15d3cb7e5001aa4b95554bbaa5e14149b
Daniil Baturin <daniil@baturin.org>
2023-03-05 23:21:03 +0000

    Improve comments in Path_options

--- src/path_options.ml
+++ src/path_options.ml
@@ -1,40 +1,76 @@
+(* Soupault allows the user to restrict widgets and hooks to a subset of all pages:
+   the user can tell them to run only on certain pages, exclude certain pages,
+   or mix those criteria.
+
+   - page/exclude page — for matching exact pages.
+   - section/exclude_section — for matching all pages within a directory
+   - path_regex/exclude_path_regex — for matching arbitrary sets of pages using a regular expression
+
+   Those options can be combined, and if they all are present in the same widget/hook,
+   then the page is checked against every option,
+   i.e., the page is excluded if it matches any exclude_* option
+   and considered included if it matches any inclusion option
+   (in other words, there is no inclusion option precedence).
+
+   One more thing to note is that the [section] option only applies to files in a directory
+   but not to its subdirectories.
+   To make a widget apply to a directory tree, the user needs to add [include_subsections=true].
+  *)
+
 open Defaults
 open Soupault_common
 
+(* The underlying function of the [page/exclude_page] option.
+   [page = "foo/bar.html"] enabled widget/hook iff
+   the page source path is "foo/bar.html".
+ *)
 let page_matches site_dir actual_path conf_path =
   let conf_path = FilePath.concat site_dir conf_path in
   (=) conf_path actual_path
 
+(* The underlying function of the [pathregex/exclude_path_regex] option. *)
 let regex_matches actual_path path_re =
   try Regex_utils.Raw.matches ~regex:path_re actual_path
   with Regex_utils.Bad_regex ->
     soupault_error @@ Printf.sprintf {|Could not check a path regex option: malformed regex "%s"|} path_re
 
-(* Normally a directory is a section.
-   However, some directories can be in fact hand-made "clean URLs".
+(* The underlying function of the [section/exclude_section] option.
+
+   Section check is more complicated because it needs to tell apart directories that are actually sections
+   and hand-made clean URLs.
+   (Hugo calls them "branch bundles" and "leaf bundles").
+
+   Normally, a directory is a section.
+   However, web servers have no distinction and the user may want to create foo/index.html instead of site/foo.html.
    One valid reason to do that is to keep page-specific assets together with pages.
 
-   We provide a way to mark a directory as "leaf rather than branch" using a configurable "leaf file".
- *)
-let has_leaf_file settings page_file =
-  match settings.index_leaf_file with
-  | None -> false
-  | Some lf ->
-    FileUtil.test FileUtil.Exists @@ FilePath.concat (FilePath.dirname page_file) lf
+   Soupault provides two ways to mark a directory as a leaf rather than a branch.
+   The first way is to use a "leaf file".
+   If that file is present, the directory is treated as a hand-made clean URL.
+   By default the leaf file is ".leaf" but it's configurable in [index.leaf_file].
 
+   The second way is to add a regex for pages that must be treated as content pages rather than section index pages
+   to [index.force_indexing_path_regex].
+   Pages that match that regex are treated as content pages regardless of their names.
+ *)
 let is_handmade_clean_url settings page_file =
+  (* Checks if a leaf file exists. *)
+  let has_leaf_file settings page_file =
+    match settings.index_leaf_file with
+    | None -> false
+    | Some lf ->
+      FileUtil.test FileUtil.Exists @@ FilePath.concat (FilePath.dirname page_file) lf
+  in
   let page_name = FilePath.basename page_file |> FilePath.chop_extension in
-  (* If it doesn't look like an index page, then there's no question: it's a normal page. *)
+  (* If it doesn't look like an index page, then there's no question: it's a normal (content) page. *)
   if (page_name <> settings.index_page) then false else
-  (* If a page is in the forced_indexing_path_regex, we treat it as a normal page regardless of its file name. *)
+  (* If a page matches index.forced_indexing_path_regex, we treat it as a normal page regardless of its file name. *)
   if (List.exists (regex_matches page_file) settings.index_force) then
     let () = Logs.debug @@ fun m -> m "Forced indexing is enabled for page %s" page_file in
     true
-  (* Another way to force an "index" page to be treated as a normal page
-     is to create a "leaf marker" file in its directory,
-     if enabled by the index.leaf_file option. *)
+  (* And if it contains a leaf file, then we treat it as a content page as well. *)
   else if (has_leaf_file settings page_file) then
-    let () = Logs.debug @@ fun m -> m "Directory with page %s contains a leaf marker file, treating as a non-index page" page_file in
+    let () = Logs.debug @@ fun m -> m "Directory with page %s contains a leaf marker file, treating it as a non-index page" page_file in
     true
   else false
 
@@ -47,9 +83,10 @@ let section_matches ?(include_subsections=false) settings site_dir actual_path c
      then FilePath.dirname page_dir
      else page_dir
    in
-   (* is_subdir doesn't consider a dir its own subdir, so we need to handle the same dir case explicitly. *)
-   (include_subsections && (FilePath.is_subdir page_dir conf_path))  || (conf_path = page_dir)
+   (* FilePath.is_subdir doesn't consider a dir its own subdir, so we need to handle that case explicitly. *)
+   (include_subsections && (FilePath.is_subdir page_dir conf_path)) || (conf_path = page_dir)
 
+(* The high-level page check function that handles all path options. *)
 let page_included settings options site_dir page_file =
   if (List.exists (regex_matches page_file) options.regexes_exclude) ||
      (List.exists (page_matches site_dir page_file) options.pages_exclude)  ||
@@ -62,10 +99,18 @@ let page_included settings options site_dir page_file =
     (List.exists (page_matches site_dir page_file) options.pages) ||
     (List.exists (section_matches ~include_subsections:options.include_subsections settings site_dir page_file) options.sections)
 
+(* The default behavior for an index page foo/index.html is to include
+   everything under [foo/*.*] and [foo/*/*.*] in its view
+   (i.e. "current section and all its subsections").
+
+   This function checks if there are any significant options set for a view
+   or its config is effectively default.
+ *)
 let is_default opts =
-  (* The include_subsections option only makes sense when section/exclude_section is also set.
-     A config with just include_subsection but no other options is considered default,
-     for the decision whether to use page targeting options or not.
+  (* [include_subsections=true] option only makes a difference when [section/exclude_section] is also set —
+     otherwise it's implied.
+     A config with just [include_subsections] but no other options is thus considered default,
+     for the purpose of deciding whether to use page targeting options or not.
    *)
   let opts = {opts with include_subsections=default_path_options.include_subsections} in
   opts = default_path_options
