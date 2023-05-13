4575538f0a6ac64783acc7466eea2c1c9bf2f8d4
Daniil Baturin <daniil@baturin.org>
2023-05-13 22:18:08 +0100

    Improve comments in the Widgets module

--- src/widgets.ml
+++ src/widgets.ml
@@ -9,7 +9,7 @@ type 'a widget = {
   func: Defaults.env -> Otoml.t -> 'a Soup.node -> (unit, string) result
 }
 
-(* The widgets structure is widget priority list plus a hash with actual widgets *)
+(* The widgets datastructure is a widget priority list plus a hash with actual widgets *)
 type 'a widgets = string list * (string, 'a widget) Hashtbl.t
 
 (* Option monad *)
@@ -22,6 +22,10 @@ let find_widget plugins name =
   let builtin_w = List.assoc_opt name Builtin_widgets.widgets in
   match plugin_w, builtin_w with
   | Some p, Some _ ->
+    (* We intentionally allow users to replace built-in widgets with plugins,
+       if they want to.
+       It may create some confusion for people who try to use someone else's setup
+       but I believe the benefits to flexibility outweigh that. *)
     let () = Logs.warn @@ fun m -> m "Widget name %s is redefined by a plugin" name in
     Some p
   | Some p, None -> Some p
@@ -111,9 +115,9 @@ let get_widget_order hash =
   | Tsort.Sorted ws -> Ok ws
   | Tsort.ErrorCycle ws -> Error (Printf.sprintf "Found a circular dependency between widgets: %s" (String.concat " " ws))
 
-(** Splits the ordered list of widgets into parts that should run before and after index extraction.
+(* Splits the list of widgets into parts that should run before and after index extraction.
 
-    
+   The assumption is that the [all_widgets] list is already sorted in topological order.
  *)
 let partition_widgets all_widgets index_deps =
   let rec aux index_deps before_index after_index =
@@ -160,19 +164,19 @@ let get_widgets settings config plugins index_deps =
 
 (** Check if a widget should run or not.
 
-    If any of exlude_page, exclude_section, or exclude_path_regex options
+    If any of [exclude_page], [exclude_section], or [exclude_path_regex] options
     are present, they are checked first.
-    If any of them matches, page is excluded and widget doesn't run.
+    If any of them matches, the page is excluded and the widget doesn't run on it.
 
-    If any of page, section, or path_regex options are present,
+    If any of [page], [section], or [path_regex] options are present,
     the widget runs if any of them matches.
-    If not, page is excluded.
+    If not, the page is excluded.
 
-    page and section options take relative paths, such as "section/page.html"
+    [page] and [section] options take relative paths, such as "section/page.html"
     or "section/subsection".
-    path_regex is a Perl-compatible regex.
+    [path_regex] is a Perl-compatible regex.
 
-    If none of those options are present, widget always runs.
+    If none of those options are present, the widget always runs.
  *)
 let widget_should_run settings name widget page_file =
   let disabled = Config.find_bool_or ~default:false widget.config ["disabled"] in
