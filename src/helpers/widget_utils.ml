23bf2596d1af46cb3f6b36eb774e6108e73ba6cf
Daniil Baturin <daniil@baturin.org>
2025-04-21 19:47:23 +0100

    Remove the now-unused get_selectors function

--- src/helpers/widget_utils.ml
+++ src/helpers/widget_utils.ml
@@ -1,6 +1,3 @@
-let get_selectors config =
-  Otoml.Helpers.find_strings_opt config ["selector"] |> Option.to_result ~none:{|Missing required option "selector"|}
-
 let no_container_action selectors message =
   Logs.debug @@ fun m -> m {|Page has no elements matching selectors "%s": %s|}
     (selectors |> Utils.format_list ~quote:false) message
